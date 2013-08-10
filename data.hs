02f8fc61ded4da38a0c2a0166db8ec9dda1ca329
Kelsey <kelseyq@gmail.com>
2013-08-10 00:21:29 -0700

    use simpleHTTP to make adding a body easier it's sad how long this took me to figure out. learning me a haskell!

--- data.hs
+++ data.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric  #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards, DeriveGeneric  #-}
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.Text hiding (dropWhile)
 import Data.Aeson
 import Control.Applicative
 import Control.Monad
-import Network.HTTP.Conduit
 import GHC.Generics
 import BV
+import Data.Maybe
+import Control.Monad.IO.Class (liftIO)
+import Network.HTTP
 
 --run first:
 --	cabal install http-conduit 
@@ -17,12 +19,18 @@ key = "0373FX9sGnppdRBUxTroWBx8Rb4EJ53kFAYeMNgpvpsH1H"
 
 callServer path = "http://icfpc2013.cloudapp.net/" ++ (dropWhile ('/'==) path) ++ "?auth=" ++ key
 
-
 statsURL = callServer "status"
 trainURL = callServer "train"
 
 getJSON :: String -> IO BS.ByteString
-getJSON url = simpleHttp url
+getJSON url = do
+   json <- simpleHTTP (getRequest url) >>= getResponseBody
+   return (BS.pack json)
+
+call :: FromJSON b => String -> IO b
+call url = 
+ -- Get JSON data and decode it
+ (>>= maybe (ioError $ userError "deserialization error") return) (decode <$> (getJSON url))
 
    --interface TrainingProblem {
    --  challenge: string;
@@ -41,12 +49,24 @@ data TrainingProblem =
 instance FromJSON TrainingProblem
 instance ToJSON TrainingProblem
 
---TODO: add body to request
---testPrint = encode (TrainingProblem {Main.id = "test", Main.size=4, Main.operators = (Array ["c"]), challenge = "testtest"} )
+   --interface TrainingRequest {
+   --    size?: number;
+   --    operators?: string[];
+   --   }
 
-train = 
- -- Get JSON data and decode it
- (eitherDecode <$> (getJSON trainURL)) :: IO (Either String TrainingProblem)
+data TrainingRequest = 
+   TrainingRequest { 
+                     reqSize :: Maybe Int,
+                     reqOperators  :: Maybe Array --TODO: take operators
+                   } deriving (Show)  
+
+instance ToJSON TrainingRequest where
+  toJSON TrainingRequest{..} = object $ catMaybes
+                                           [ ("size" .=) <$> reqSize
+                                           , ("operators" .=) <$> reqOperators ]
+
+--TODO: add body to request
+train = call trainURL :: IO TrainingProblem
 
  
  --interface Problem {
