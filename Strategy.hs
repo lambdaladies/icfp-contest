25d3b42dc248ceb4d784f699e79b2dda1fe4e079
Daira Hopwood <daira@jacaranda.org>
2013-08-10 20:48:52 +0100

    Start of code to do something with myproblems.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- Strategy.hs
+++ Strategy.hs
@@ -1,10 +1,17 @@
 module Strategy where
 
+import System.IO (FilePath, readFile)
+import Control.Monad
 import BV
 import API
 
-cost_threshold timeLeft = (toInteger timeLeft * 100) `min` 1000000
+cost_threshold (Just timeLeft) = (toInteger timeLeft * 100) `min` 1000000
+cost_threshold Nothing         = 1000000
 
 should_we_solve problem =
     not (problemSolved problem) &&
     l_generate (problemOperators problem) (problemSize problem) < cost_threshold (problemTimeLeft problem)
+
+main = do
+    json <- readFile "myproblems.txt"
+    print json
