a3f10677a35d0e62fe71fe776f0b8dd7c1c39905
Sandra Dylus <ichistmeinname@web.de>
2013-08-11 00:39:16 +0200

    Add Test.hs

---
+++ Test.hs
@@ -0,0 +1,15 @@
+module Test where
+
+import BV
+import API
+
+------- GENERATE
+testNot  = Operators [Not] [] []
+testPlus = Operators [] [Plus] []
+testIf   = Operators [] [] [IfZero]
+testSimple = Operators [Not] [Plus] []
+
+------- TRAINING
+testRequest = TrainingRequest (Just 10) []
+
+testTraining = testPost testRequest
