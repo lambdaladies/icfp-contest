f76adbc649c67876c363e1567aca26f81faae942
Sandra Dylus <ichistmeinname@web.de>
2013-08-11 00:15:29 +0200

    Add function to post data for a TrainingRequest and adjust ToJSON and FromJSON instances for TrainingProblems and TrainingRequests

--- data.hs
+++ data.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards, DeriveGeneric  #-}
+
+module Data where
+
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.Text hiding (dropWhile)
 import Data.Aeson
@@ -9,15 +12,35 @@ import BV
 import Data.Maybe
 import Control.Monad.IO.Class (liftIO)
 import Network.HTTP
+import Network.HTTP.Types (methodPost)
+import Network.HTTP.Conduit
+import qualified Data.ByteString.Lazy as L
 
 --run first:
---	cabal install http-conduit 
--- 	cabal install aeson
+--    cabal install http-conduit
+--    cabal install aeson
 
 --hey remove this before making code public!!!!
 key = "0373FX9sGnppdRBUxTroWBx8Rb4EJ53kFAYeMNgpvpsH1H"
 
 callServer path = "http://icfpc2013.cloudapp.net/" ++ (dropWhile ('/'==) path) ++ "?auth=" ++ key
+
+postData :: TrainingRequest -> IO TrainingProblem
+postData jsonBody = do
+  initRequest <- parseUrl trainURL
+  let request = initRequest { method = methodPost
+                            , requestHeaders = [("Content-Type", "application/json")]
+                            , requestBody = RequestBodyLBS (encode jsonBody)
+                            }
+  res <- withManager $ httpLbs request
+  let eitherJson = eitherDecode (responseBody res)
+  case eitherJson of
+    Left e -> error ("Could not decode: " ++ show (responseBody res)
+              ++ "\n" ++ show e)
+    Right training -> return training
+
+testPost :: TrainingRequest -> IO ()
+testPost jsonBody = postData jsonBody >>= print
 
 statsURL = callServer "status"
 trainURL = callServer "train"
@@ -28,7 +51,7 @@ getJSON url = do
    return (BS.pack json)
 
 call :: FromJSON b => String -> IO b
-call url = 
+call url =
  -- Get JSON data and decode it
  (>>= maybe (ioError $ userError "deserialization error") return) (decode <$> (getJSON url))
 
@@ -39,36 +62,43 @@ call url =
    --  operators: string[];
    --}
 
-data TrainingProblem = 
-   TrainingProblem { id    		:: String,
-   					 size        :: Int,
-   					 operators	:: Array, --TODO: parse into operators
-   					 challenge   :: String
-                   } deriving (Show, Generic)
+data TrainingProblem =
+   TrainingProblem { id        :: String,
+                     size      :: Int,
+                     operators :: [Ops],
+                     challenge :: String -- TODO: parse into expression
+                    } deriving (Show, Generic)
 
 instance FromJSON TrainingProblem
 instance ToJSON TrainingProblem
+
+instance FromJSON Ops where
+  parseJSON (String s) = return $ read $ unpack s
+  parseJSON op = error ("failed to parse operator: " ++ show op)
+
+instance ToJSON Ops where
+  toJSON ops = String (pack $ show ops)
 
    --interface TrainingRequest {
    --    size?: number;
    --    operators?: string[];
    --   }
 
-data TrainingRequest = 
-   TrainingRequest { 
-                     reqSize :: Maybe Int,
-                     reqOperators  :: Maybe Array --TODO: take operators
-                   } deriving (Show)  
+data TrainingRequest =
+   TrainingRequest {
+                     reqSize       :: Maybe Int,
+                     reqOperators  :: [Ops]
+                   } deriving (Show)
 
 instance ToJSON TrainingRequest where
-  toJSON TrainingRequest{..} = object $ catMaybes
-                                           [ ("size" .=) <$> reqSize
-                                           , ("operators" .=) <$> reqOperators ]
+  toJSON tReq = case reqSize tReq of
+    Nothing -> object ["operators" .= (toJSON (reqOperators tReq))]
+    Just size -> object [ "size" .= size
+                        , "operators" .= (toJSON (reqOperators tReq)) ]
 
 --TODO: add body to request
 train = call trainURL :: IO TrainingProblem
 
- 
  --interface Problem {
  --   id: string;
  --   size: number;
@@ -76,6 +106,3 @@ train = call trainURL :: IO TrainingProblem
  --   solved?: boolean;
  --   timeLeft?: number
  -- }
-
-
-
